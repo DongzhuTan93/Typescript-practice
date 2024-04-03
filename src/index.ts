1dfc8a0c2cbe8cfeab8bced1733994b7f97167cf
Dongzhu Tan <dt222ha@student.lnu.se>
2024-04-03 16:29:28 +0200

    Creating Classes and Objects, readOnly and optional properties

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+class Account {
+    readonly id: number;
+    owner: string;
+    balance: number;
+    nickname?: string;
+
+    constructor (id: number, owner : string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+        
+    }
+}
+
+
+let account = new Account(1, 'dong', 0 )
+account.deposit(100)
+console.log(account.balance)
+console.log(typeof account)
+console.log(account instanceof Account)
+
+
+
