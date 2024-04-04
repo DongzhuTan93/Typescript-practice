6bcb7ee7fb8ac7238b479c9408f2bfae83a813bb
Dongzhu Tan <dt222ha@student.lnu.se>
2024-04-04 14:57:34 +0200

    Inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
-class Ride {
-    // passaanger
-    // pickupLocation
-    // dropOffLocation
+class Person {
+    constructor (public firstname: string, public lastname: string) { }
 
-    private static _activeRides: number = 0 // static properties is what we need is a singel or global place where we can keep track of the active rides.
-                                   // A static properties is a property that belongs to a class not en object, so we gonna have only one instance of that property in memory
-                                // private method to prevent to asign new value to activeRides outside the class
+    get fullName() {
+        return this.firstname + ' ' + this.lastname
+    }
 
-    start () { Ride._activeRides++    } 
-    stop () { Ride._activeRides--     }
+    walk() {
+        console.log('Walking')
+    }
+    
+} 
 
+class Student extends Person {
+    constructor(public studentID: number, firtName: string, lastName: string) {
+        super(firtName, lastName)
+    }
 
-    static get activeRides() {
-        return Ride._activeRides
+    takeTest() {
+        console.log('Take a test')
     }
 }
 
+let student = new Student (1, 'dong', 'tan')
 
-
-let ride1 = new Ride() // before static method we will get only one same result (1) but we have two active rides even it is two separate objects
-ride1.start()
-
-let ride2 = new Ride() // because each object is separate space in memory, so each object is independently tracking the active rides.
-ride2.start()
-
-console.log(Ride.activeRides) // now activeRides belongs to ride class not en ride object
-
-
-// When we make a method or a property static , that property or method becomes part of class and will have only a single instance os them in memory
