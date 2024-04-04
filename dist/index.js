a5d9c2bda573b1c2d3d559c8b0bc51df233d85ed
Dongzhu Tan <dt222ha@student.lnu.se>
2024-04-04 15:40:25 +0200

    Polymorphism

--- dist/index.js
+++ dist/index.js
@@ -20,12 +20,23 @@ class Student extends Person {
         console.log('Take a test');
     }
 }
-let student = new Student(1, 'dong', 'tan');
 class Teacher extends Person {
     get fullName() {
         return 'Professor ' + super.fullName;
     }
 }
-let teacher = new Teacher('Dong', 'tan');
-console.log(teacher.fullName);
+class Principal extends Person {
+    get fullName() {
+        return 'principal ' + super.fullName;
+    }
+}
+function printNames(people) {
+    for (let person of people)
+        console.log(person.fullName);
+}
+printNames([
+    new Student(1, 'dong', 'tan'),
+    new Teacher('Tan', 'dong'),
+    new Principal('Dong', 'zhu')
+]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -21,15 +21,35 @@ class Student extends Person {
     }
 }
 
-let student = new Student (1, 'dong', 'tan')
-
-
+//let student = new Student (1, 'dong', 'tan')
 
 class Teacher extends Person {
     override get fullName() {
-        return 'Professor ' + super.fullName // tell the complier that we have change the method
+        return 'Professor ' + super.fullName // Overide method tell the complier that we have change the method
     }
 }
 
-let teacher = new Teacher ('Dong', 'tan')
-console.log(teacher.fullName)
+
+class Principal extends Person {
+    override get fullName() {
+        return 'principal ' + super.fullName 
+    }
+}
+
+//let teacher = new Teacher ('Dong', 'tan')
+//console.log(teacher.fullName)
+
+
+
+function printNames (people: Person[]) {
+    for (let person of people)
+        console.log(person.fullName)
+}
+
+
+printNames ([
+    new Student (1, 'dong', 'tan'),
+    new Teacher ('Tan', 'dong'),
+    new Principal ('Dong', 'zhu')
+
+])
