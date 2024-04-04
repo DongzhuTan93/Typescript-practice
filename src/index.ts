658d9b1896e850e83872c64fecbd344c2417bcbb
Dongzhu Tan <dt222ha@student.lnu.se>
2024-04-04 11:29:20 +0200

    Getters and setters

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ class Account {
     // private _balance: number;
     nickname?: string;
 
-    constructor (public readonly id: number, public owner : string, private _balance: number) {
+    constructor (
+        public readonly id: number, 
+        public owner : string, 
+        private _balance: number) {
         //this.id = id;
         //this.owner = owner;
         //this._balance = balance;
@@ -16,9 +19,16 @@ class Account {
         this._balance += amount; 
     }
 
-    getBalance () {
+    get balance (): number { // getter is a method inside a class that we use for getting the value of a property
         return this._balance
     }
+
+    set balance (value: number) {
+        if (value < 0)
+            throw new Error ('Invalid value')
+        this._balance = value
+    }
+
 
     /*private method () {
         return this.owner
@@ -31,7 +41,8 @@ let account = new Account(1, 'dong', 0 )
 account.deposit(100)
 console.log(account.owner)
 
-console.log(account.getBalance()) // but through getBalance() method you can call this.balance private properties
+console.log(account.balance) // when you do not want to call a method here, so we chang it with 'get' key word and we can access the balance property just like before.
+account.balance = 1 // just now we can not assign a value to balance, we need a 'set' kty word.
 
 console.log(typeof account)
 console.log(account instanceof Account)
