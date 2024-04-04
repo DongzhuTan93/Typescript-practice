2ae2e975c1753bea3c75d305545f833691ff4633
Dongzhu Tan <dt222ha@student.lnu.se>
2024-04-04 17:09:12 +0200

    Abstract methods do not have a body

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 /*abstract class Calendar {
     constructor(public name: string) {}
 
-    abstract  addEvent(): void
+    abstract  addEvent(): void   // Abstract methods don't have a body
     abstract removeEvent(): void
 }*/
 
 
-interface Calendar { // interface is a simpler abstract class? usinf interface we can describe the shape of en object
+interface Calendar { // interface is a simpler abstract class? usinf interface we can describe the shape of en object. Protected members are inherited. Private members are not. 
     name: string
     addEvent(): void
     removeEvent(): void
